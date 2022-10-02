d54742c5935efdf7702fc99e69b27906f8fcb60f
Rares Tamasanu <rares.ioan@icloud.com>
2022-10-03 00:03:18 +0100

    Solve assignment

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(n):
+    for d in range(2, int( n/2 )):
+        if n % d == 0:
+            return False
+
+    return True
+
 def primes(number_of_primes):
     list = []
+    if number_of_primes > 0:
+        list.append(2)
+        number_of_primes -= 1
+    else:
+        raise ValueError
+
+    next = 3 
+    while number_of_primes != 0:
+        if is_prime(next):
+            list.append(next)
+            number_of_primes -= 1
+        next += 2
+
     return list
+
+print(primes(10))
